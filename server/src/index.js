5d06e072264b37e734ce238f0d8a4c55857761b2
Rupak Lamsal <Rupak.Lamsal@libertymutual.com>
2019-07-06 23:17:49 -0400

    combined  app and index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,27 @@
+const { ApolloServer } = require('apollo-server');
+const { importSchema } = require('graphql-import');
+
+const resolvers = require('./resolvers/resolvers');
 const { connectToDB } = require('./utils/database');
 const logger = require('./utils/logger');
-const app = require('./app');
 
 require('dotenv').config();
+
+const typeDefs = importSchema('src/schemas/schema.graphql');
+
+const isProduction = process.env.APP_ENV && process.env.APP_ENV === 'production';
+
+const app = new ApolloServer({
+  typeDefs,
+  resolvers,
+  ...(!isProduction && {
+    playground: true,
+    introspection: true,
+  }),
+});
 
 const port = process.env.PORT || 4000;
 
 connectToDB()
-  .then(() => app().listen(port, () => logger.info(`🚀  Server running on port ${port}`)))
+  .then(() => app.listen(port, () => logger.info(`🚀  Server running on port ${port}`)))
   .catch(err => logger.error('Error starting server: ', err));
